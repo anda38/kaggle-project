a9dc465684de98c4e34fc7caf22bbed47b27f019
anda38 <andalouse38@icloud.com>
2025-10-13 10:27:44 +0200

    created jupyter notebook added libraries

--- kaggle.py
+++ kaggle.py
@@ -1,2 +1,10 @@
-x=1+18
-print(x)
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+census = pd.read_csv("/Users/andalouse/data/census_starter.csv")
+
+census.head(10)
+
